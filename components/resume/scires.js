eec60cee50e4cef6fce6850a12dee6ca1f1ab2da
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-05 19:04:22 -0400

    Update scires.js

--- components/resume/scires.js
+++ components/resume/scires.js
@@ -2,22 +2,28 @@ import { Image, Center } from "@chakra-ui/react";
 
 const Scires = () => {
   return (
-    <Center pt={70}>
-      <Image
-        boxSize="800px"
-        src="/images/sci_1.jpg"
-        alt="Resume"
-        width={600}
-        border="5px solid grey"
-      />
-      <Image
-        boxSize="800px"
-        src="/images/sci_2.jpg"
-        alt="Resume"
-        width={600}
-        border="5px solid grey"
-      />
-    </Center>
+    <div>
+      <Center pt={70}>
+        <Image
+          boxSize="800px"
+          src="/images/sci_1.jpg"
+          alt="Resume"
+          width={{ base: "90%", md: "70%", lg: "40%" }}
+          height={{ base: "auto", md: "auto", lg: "auto" }}
+          border="5px solid grey"
+        />
+      </Center>
+      <Center>
+        <Image
+          boxSize="800px"
+          src="/images/sci_2.jpg"
+          alt="Resume"
+          width={{ base: "90%", md: "70%", lg: "40%" }}
+          height={{ base: "auto", md: "auto", lg: "auto" }}
+          border="5px solid grey"
+        />
+      </Center>
+    </div>
   );
 };
 
