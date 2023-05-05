ca4e1b3c5b7f75dee705ffdb3dcefd4e887ec90b
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-05 19:02:40 -0400

    Update code.js

--- components/homepage/code.js
+++ components/homepage/code.js
@@ -5,10 +5,26 @@ const Codes = () => {
   return (
     <div>
       <Stack direction="row">
-        <Code colorScheme="red" children="console.log('welcome');" />
-        <Code colorScheme="red" children="print('welcome')" />
-        <Code colorScheme="red" children="printf('welcome');" />
-        <Code colorScheme="red" children="System.out.println('welcome');" />
+        <Code
+          colorScheme="red"
+          children="console.log('welcome');"
+          fontSize={{ base: "8.5px", md: "10px", lg: "15px" }}
+        />
+        <Code
+          colorScheme="red"
+          children="print('welcome')"
+          fontSize={{ base: "8.5px", md: "10px", lg: "15px" }}
+        />
+        <Code
+          colorScheme="red"
+          children="printf('welcome');"
+          fontSize={{ base: "8.5px", md: "10px", lg: "15px" }}
+        />
+        <Code
+          colorScheme="red"
+          children="System.out.println('welcome');"
+          fontSize={{ base: "8.5px", md: "10px", lg: "15px" }}
+        />
       </Stack>
     </div>
   );
