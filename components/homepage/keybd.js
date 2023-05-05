9fc0b257d1b850908fca319d482e9e9cba5f70eb
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-05 19:03:02 -0400

    Update keybd.js

--- components/homepage/keybd.js
+++ components/homepage/keybd.js
@@ -3,8 +3,12 @@ import { Kbd } from "@chakra-ui/react";
 const Keyboard = () => {
   return (
     <span>
-      <Kbd>control</Kbd> + <Kbd>shift</Kbd> + <Kbd>Q</Kbd> / <Kbd>Alt</Kbd> +{" "}
-      <Kbd>F4</Kbd> + <Kbd>Q</Kbd>
+      <Kbd fontSize={{ base: "10px", md: "12px", lg: "15px" }}>control</Kbd> +{" "}
+      <Kbd fontSize={{ base: "10px", md: "12px", lg: "15px" }}>shift</Kbd> +{" "}
+      <Kbd fontSize={{ base: "10px", md: "12px", lg: "15px" }}>Q</Kbd> /{" "}
+      <Kbd fontSize={{ base: "10px", md: "12px", lg: "15px" }}>Alt</Kbd> +{" "}
+      <Kbd fontSize={{ base: "10px", md: "12px", lg: "15px" }}>F4</Kbd> +{" "}
+      <Kbd fontSize={{ base: "10px", md: "12px", lg: "15px" }}>Q</Kbd>
     </span>
   );
 };
