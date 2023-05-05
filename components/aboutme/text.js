f335051923b93f24565d2dc618585c91b0d48643
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-05 19:00:48 -0400

    Update text.js

--- components/aboutme/text.js
+++ components/aboutme/text.js
@@ -1,11 +1,11 @@
-import { Center, Text, Divider, Link } from "@chakra-ui/react";
+import { Center, Text, Divider, Link, Box } from "@chakra-ui/react";
 
 const Titles = () => {
   return (
     <div>
       <Center>
         <Text
-          fontSize="6xl"
+          fontSize={{ base: "35px", md: "40px", lg: "50px" }}
           spacing={3}
           pt={50}
           fontWeight={"bold"}
@@ -18,10 +18,10 @@ const Titles = () => {
       <Divider />
       <Center>
         <Text
-          fontSize="4xl"
+          fontSize={{ base: "30px", md: "35px", lg: "40px" }}
           spacing={3}
           pt={50}
-          pl={15}
+          //   pl={15}
           fontWeight={"bold"}
           fontFamily={"serif"}
         >
@@ -31,56 +31,68 @@ const Titles = () => {
       </Center>
 
       <Center>
-        <Text fontSize="2xl" spacing={3} pt={10} pl={30} fontFamily={"serif"}>
-          {" "}
-          My name is Austin, a second year student in Co-op Computer Science and
-          Biochemistry at University of Toronto.
-        </Text>
+        <Box>
+          <Text
+            fontSize={{ base: "17px", md: "20px", lg: "23px" }}
+            spacing={3}
+            pt={10}
+            pl={30}
+            fontFamily={"serif"}
+          >
+            {" "}
+            My name is Austin, a second year student in Co-op Computer Science
+            and Biochemistry at University of Toronto.
+          </Text>
+        </Box>
+      </Center>
+
+      <Center>
+        <Box>
+          <Text
+            fontSize={{ base: "17px", md: "20px", lg: "23px" }}
+            spacing={3}
+            pt={10}
+            paddingLeft={{ base: 0, md: 10, lg: 40 }}
+            paddingRight={{ base: 0, md: 10, lg: 40 }}
+            fontFamily={"serif"}
+          >
+            {" "}
+            I was born in Shanghai, and moved to Tokyo afterwards. Growing up in
+            two culturally rich and diverse cities, I have developed a deep
+            appreciation for the nuances of language, tradition, and history.
+            And I was lucky to have the opportunity to learn Mandarin, Japanese,
+            and English, and are fluent in all three languages.
+          </Text>
+        </Box>
+      </Center>
+
+      <Center>
+        <Box>
+          <Text
+            fontSize={{ base: "17px", md: "20px", lg: "23px" }}
+            spacing={0}
+            pt={10}
+            paddingLeft={{ base: 0, md: 10, lg: 40 }}
+            paddingRight={{ base: 0, md: 10, lg: 40 }}
+            fontFamily={"serif"}
+          >
+            {" "}
+            Interestingly, I would often spend hours tinkering with gadgets and
+            exploring the inner workings of machines. My curiosity about how
+            things worked only grew with time, and eventually led me to pursue a
+            double major in Computer Science and Biochemistry at the University
+            of Toronto.
+          </Text>
+        </Box>
       </Center>
 
       <Center>
         <Text
-          fontSize="2xl"
+          fontSize={{ base: "17px", md: "20px", lg: "23px" }}
           spacing={0}
           pt={10}
-          pl={40}
-          mr={40}
-          fontFamily={"serif"}
-        >
-          {" "}
-          I was born in Shanghai, and moved to Tokyo afterwards. Growing up in
-          two culturally rich and diverse cities, I have developed a deep
-          appreciation for the nuances of language, tradition, and history. And
-          I was lucky to have the opportunity to learn Mandarin, Japanese, and
-          English, and are fluent in all three languages.
-        </Text>
-      </Center>
-
-      <Center>
-        <Text
-          fontSize="2xl"
-          spacing={0}
-          pt={10}
-          pl={40}
-          mr={40}
-          fontFamily={"serif"}
-        >
-          {" "}
-          Interestingly, I would often spend hours tinkering with gadgets and
-          exploring the inner workings of machines. My curiosity about how
-          things worked only grew with time, and eventually led me to pursue a
-          double major in Computer Science and Biochemistry at the University of
-          Toronto.
-        </Text>
-      </Center>
-
-      <Center>
-        <Text
-          fontSize="2xl"
-          spacing={0}
-          pt={10}
-          pl={40}
-          mr={40}
+          paddingLeft={{ base: 0, md: 10, lg: 40 }}
+          paddingRight={{ base: 0, md: 10, lg: 40 }}
           fontFamily={"serif"}
         >
           I have had the opportunity to explore my interests in both fields
@@ -98,10 +110,6 @@ const Titles = () => {
       </Center>
       <br></br>
 
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
       <br></br>
       <br></br>
     </div>
