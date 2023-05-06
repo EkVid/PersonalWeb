4128f43dfda8a151e4dc7738cb502499c1a55c32
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-06 11:20:29 -0400

    Add files via upload

---
+++ pages/exp_S.js
@@ -0,0 +1,11 @@
+import Bar from "../components/bar";
+import Anim_S from "../components/exp_s/anim_s";
+
+export default function Home() {
+  return (
+    <div>
+      <Bar />
+      <Anim_S />
+    </div>
+  );
+}
