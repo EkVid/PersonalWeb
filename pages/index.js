ebf977410c2de23b38aa85dcc636fdad6ae57f30
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 15:42:50 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Bar from "../components/bar";
 import Spla from "../components/homepage/moving";
+import Navbar from "../components/bar_2";
 
 export default function FirstPost() {
   return (
     <div>
-      <Bar />
+      <Navbar />
       <Spla />
     </div>
   );
