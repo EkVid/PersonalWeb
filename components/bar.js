54bc5211f39b1988ed83949a5c00c4e50d3fb842
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 15:37:54 -0400

    Update bar.js

--- components/bar.js
+++ components/bar.js
@@ -2,7 +2,6 @@ import { useColorModeValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { useDisclosure } from "@chakra-ui/react";
 import React from "react";
-import dynamic from "next/dynamic";
 import Color_change from "./color_change";
 import { chakra } from "@chakra-ui/react";
 import {
@@ -20,6 +19,7 @@ import {
   VStack,
   CloseButton,
   Button,
+  useColorMode,
 } from "@chakra-ui/react";
 import {
   AiOutlineMenu,
@@ -27,14 +27,17 @@ import {
   AiFillGithub,
   AiFillLinkedin,
 } from "react-icons/ai";
+import { motion } from "framer-motion";
 import { VisuallyHidden } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
-
+import dynamic from "next/dynamic";
 import { Menu, MenuButton, MenuList, MenuItem, Portal } from "@chakra-ui/react";
 
 const Bar = () => {
   const bg = useColorModeValue("white", "gray.800");
   const mobileNav = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <React.Fragment>
       <chakra.header
@@ -47,54 +50,155 @@ const Bar = () => {
         py={4}
         shadow="md"
       >
-        <Flex alignItems="center" justifyContent="space-between" mx="auto">
-          <HStack display="flex" spacing={3} alignItems="center">
-            <Box
-              display={{
-                base: "inline-flex",
-                md: "none",
-              }}
-            >
-              <IconButton
+        <motion.div
+          className="title"
+          initial={{ y: -250 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 0, type: "spring", stiffness: 70 }}
+        >
+          <Flex alignItems="center" justifyContent="space-between" mx="auto">
+            <HStack display="flex" spacing={3} alignItems="center">
+              <Box
                 display={{
-                  base: "flex",
+                  base: "inline-flex",
                   md: "none",
                 }}
-                aria-label="Open menu"
-                fontSize="20px"
-                color="gray.800"
-                _dark={{
-                  color: "inherit",
-                }}
-                variant="ghost"
-                icon={<AiOutlineMenu />}
-                onClick={mobileNav.onOpen}
-              />
-              <VStack
-                pos="absolute"
-                top={0}
-                left={0}
-                right={0}
-                display={mobileNav.isOpen ? "flex" : "none"}
-                flexDirection="column"
-                p={2}
-                pb={4}
-                m={2}
-                bg={bg}
-                spacing={3}
-                rounded="sm"
-                shadow="sm"
               >
-                <CloseButton
-                  aria-label="Close menu"
-                  justifySelf="self-start"
-                  onClick={mobileNav.onClose}
+                <IconButton
+                  display={{
+                    base: "flex",
+                    md: "none",
+                  }}
+                  aria-label="Open menu"
+                  fontSize="20px"
+                  color="gray.800"
+                  _dark={{
+                    color: "inherit",
+                  }}
+                  variant="ghost"
+                  icon={<AiOutlineMenu />}
+                  onClick={mobileNav.onOpen}
                 />
+                <VStack
+                  pos="absolute"
+                  top={0}
+                  left={0}
+                  right={0}
+                  display={mobileNav.isOpen ? "flex" : "none"}
+                  flexDirection="column"
+                  p={2}
+                  pb={4}
+                  m={2}
+                  bg={bg}
+                  spacing={3}
+                  rounded="sm"
+                  shadow="sm"
+                >
+                  <CloseButton
+                    aria-label="Close menu"
+                    justifySelf="self-start"
+                    onClick={mobileNav.onClose}
+                  />
+                  <Link href="/">
+                    <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
+                      HomePage
+                    </Button>
+                  </Link>
+                  <Link href="/aboutme">
+                    <Button variant="ghost" leftIcon={<InfoIcon />} size="sm">
+                      About Me
+                    </Button>
+                  </Link>
+                  <Menu>
+                    <Button
+                      variant="ghost"
+                      // colorScheme="brand"
+                      leftIcon={<AttachmentIcon />}
+                      size="sm"
+                    >
+                      <MenuButton> Resume </MenuButton>
+                      <Portal>
+                        <MenuList>
+                          <Link href="/resumetech">
+                            <MenuItem>Tech Related</MenuItem>
+                          </Link>
+                          <Link href="/resumesci">
+                            <MenuItem>Science Related</MenuItem>
+                          </Link>
+                        </MenuList>
+                      </Portal>
+                    </Button>
+                  </Menu>
+                  <Link href="/exp_S">
+                    <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
+                      Experiences
+                    </Button>
+                  </Link>
+                  <Link href="/publications">
+                    <Button
+                      w="full"
+                      variant="ghost"
+                      //colorScheme="brand"
+                      leftIcon={<EditIcon />}
+                    >
+                      Publications
+                    </Button>
+                  </Link>
+                  <Link href="/contact">
+                    <Button
+                      w="full"
+                      variant="ghost"
+                      //colorScheme="brand"
+                      leftIcon={<ChatIcon />}
+                    >
+                      Contact
+                    </Button>
+                  </Link>
+                  <Link
+                    href="https://github.com/EkVid"
+                    leftIcon={<CheckCircleIcon />}
+                    size="sm"
+                  >
+                    <Button variant="ghost" size="sm">
+                      {" "}
+                      <AiFillGithub size={25} />
+                    </Button>
+                  </Link>
+                  <Link
+                    href="https://www.linkedin.com/in/austin-yang-3544061ba/"
+                    leftIcon={<CheckCircleIcon />}
+                    size="sm"
+                  >
+                    <Button variant="ghost" size="sm">
+                      {" "}
+                      <AiFillLinkedin size={25} />
+                    </Button>
+                  </Link>
+                </VStack>
+              </Box>
+              <chakra.a
+                href="/"
+                title="Home Page"
+                display="flex"
+                alignItems="center"
+              >
+                <VisuallyHidden>Choc</VisuallyHidden>
+                <Avatar size="sm" name="Austin" src="/images/profile.jpg" />
+              </chakra.a>
+
+              <HStack
+                spacing={5}
+                display={{
+                  base: "none",
+                  md: "inline-flex",
+                }}
+              >
                 <Link href="/">
-                  <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
+                  <Button variant="ghost" leftIcon={<AiFillHome />} size="sm">
                     HomePage
                   </Button>
                 </Link>
+
                 <Link href="/aboutme">
                   <Button variant="ghost" leftIcon={<InfoIcon />} size="sm">
                     About Me
@@ -120,35 +224,33 @@ const Bar = () => {
                     </Portal>
                   </Button>
                 </Menu>
-                <Link href="/exp_S">
-                  <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
+
+                <Link href="/experiences">
+                  <Button
+                    variant="ghost"
+                    leftIcon={<CheckCircleIcon />}
+                    size="sm"
+                  >
                     Experiences
                   </Button>
                 </Link>
+
                 <Link href="/publications">
-                  <Button
-                    w="full"
-                    variant="ghost"
-                    //colorScheme="brand"
-                    leftIcon={<EditIcon />}
-                  >
+                  <Button variant="ghost" leftIcon={<EditIcon />} size="sm">
                     Publications
                   </Button>
                 </Link>
+
                 <Link href="/contact">
-                  <Button
-                    w="full"
-                    variant="ghost"
-                    //colorScheme="brand"
-                    leftIcon={<ChatIcon />}
-                  >
+                  <Button variant="ghost" leftIcon={<ChatIcon />} size="sm">
                     Contact
                   </Button>
                 </Link>
+
                 <Link
                   href="https://github.com/EkVid"
-                  leftIcon={<CheckCircleIcon />}
-                  size="sm"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   <Button variant="ghost" size="sm">
                     {" "}
@@ -157,115 +259,42 @@ const Bar = () => {
                 </Link>
                 <Link
                   href="https://www.linkedin.com/in/austin-yang-3544061ba/"
-                  leftIcon={<CheckCircleIcon />}
-                  size="sm"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   <Button variant="ghost" size="sm">
                     {" "}
                     <AiFillLinkedin size={25} />
                   </Button>
                 </Link>
-              </VStack>
-            </Box>
-            <chakra.a
-              href="/"
-              title="Home Page"
-              display="flex"
+              </HStack>
+            </HStack>
+            <HStack
+              spacing={300}
+              display={mobileNav.isOpen ? "none" : "flex"}
               alignItems="center"
             >
-              <VisuallyHidden>Choc</VisuallyHidden>
-              <Avatar size="sm" name="Austin" src="/images/profile.jpg" />
-            </chakra.a>
-
-            <HStack
-              spacing={5}
-              display={{
-                base: "none",
-                md: "inline-flex",
-              }}
-            >
-              <Link href="/">
-                <Button variant="ghost" leftIcon={<AiFillHome />} size="sm">
-                  HomePage
-                </Button>
-              </Link>
-
-              <Link href="/aboutme">
-                <Button variant="ghost" leftIcon={<InfoIcon />} size="sm">
-                  About Me
-                </Button>
-              </Link>
-              <Menu>
-                <Button
-                  variant="ghost"
-                  // colorScheme="brand"
-                  leftIcon={<AttachmentIcon />}
-                  size="sm"
-                >
-                  <MenuButton> Resume </MenuButton>
-                  <Portal>
-                    <MenuList>
-                      <Link href="/resumetech">
-                        <MenuItem>Tech Related</MenuItem>
-                      </Link>
-                      <Link href="/resumesci">
-                        <MenuItem>Science Related</MenuItem>
-                      </Link>
-                    </MenuList>
-                  </Portal>
-                </Button>
-              </Menu>
-
-              <Link href="/experiences">
-                <Button
-                  variant="ghost"
-                  leftIcon={<CheckCircleIcon />}
-                  size="sm"
-                >
-                  Experiences
-                </Button>
-              </Link>
-
-              <Link href="/publications">
-                <Button variant="ghost" leftIcon={<EditIcon />} size="sm">
-                  Publications
-                </Button>
-              </Link>
-
-              <Link href="/contact">
-                <Button variant="ghost" leftIcon={<ChatIcon />} size="sm">
-                  Contact
-                </Button>
-              </Link>
-
-              <Link
-                href="https://github.com/EkVid"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="ghost" size="sm">
-                  {" "}
-                  <AiFillGithub size={25} />
-                </Button>
-              </Link>
-              <Link
-                href="https://www.linkedin.com/in/austin-yang-3544061ba/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="ghost" size="sm">
-                  {" "}
-                  <AiFillLinkedin size={25} />
-                </Button>
-              </Link>
+              <chakra.a
+                p={3}
+                color="gray.800"
+                _dark={{
+                  color: "inherit",
+                }}
+                rounded="sm"
+                _hover={{
+                  color: "gray.800",
+                  _dark: {
+                    color: "gray.600",
+                  },
+                }}
+              ></chakra.a>
             </HStack>
-           </HStack>
-          <Color_change />
-        </Flex>
+            <Color_change />
+          </Flex>
+        </motion.div>
       </chakra.header>
     </React.Fragment>
   );
 };
 
 export default dynamic(() => Promise.resolve(Bar), { ssr: false });
-
