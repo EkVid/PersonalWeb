fec159218678855891375ffa67c9e5788b1141ec
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 15:11:02 -0400

    responsive design

--- components/homepage/introduction.js
+++ components/homepage/introduction.js
@@ -1,36 +1,56 @@
-import { Center, Text } from "@chakra-ui/react";
+import { Center, Text, Box } from "@chakra-ui/react";
 import Keyboard from "./keybd";
 
 const Introduction = () => {
   return (
     <div>
       <Center>
-        <Text
-          fontSize={{ base: "15px", md: "20px", lg: "25px" }}
-          spacing={3}
-          pt={5}
-          fontFamily={"serif"}
-        >
-          <center>
+        <Box>
+          <Text
+            fontSize={{ base: "15px", md: "20px", lg: "25px" }}
+            spacing={3}
+            pt={5}
+            fontFamily={"serif"}
+          >
             2nd year Co-op CS and Biochemistry Student at University of Toronto.
-          </center>
-          <br></br>
-          <center>
-            Self motivated to learn and explore new concepts and technologies.
-          </center>
-          <br></br>
-          Working to act as the bridge between natural science and computer
-          science
-          <br></br>
-          <center>students.</center>
-          <br></br>
-        </Text>
+          </Text>
+        </Box>
       </Center>
+      <br></br>
+      <Center>
+        <Box>
+          <Text
+            fontSize={{ base: "15px", md: "20px", lg: "25px" }}
+            spacing={3}
+            pt={5}
+            fontFamily={"serif"}
+          >
+            Self motivated to learn and explore new concepts and technologies.
+          </Text>
+        </Box>
+      </Center>
+      <br></br>
+
+      <Center>
+        <Box>
+          <Text
+            fontSize={{ base: "15px", md: "20px", lg: "25px" }}
+            spacing={3}
+            pt={5}
+            fontFamily={"serif"}
+          >
+            Working to act as the bridge between natural science and computer
+            science students.{" "}
+          </Text>
+        </Box>
+      </Center>
+      <br></br>
+      <br></br>
+
       <Center>
         <Keyboard />
       </Center>
     </div>
   );
 };
-
 export default Introduction;
