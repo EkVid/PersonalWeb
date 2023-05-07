e77f46b378dfde03c210e4f8cb939d235a88e382
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 16:49:16 -0400

    Update avatar.js

--- components/contact/avatar.js
+++ components/contact/avatar.js
@@ -10,173 +10,173 @@ import {
 } from "@chakra-ui/react";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillPhone, AiFillMail, AiFillLinkedin } from "react-icons/ai";
-import { Menu, MenuButton, Portal, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  Portal,
+  MenuItem,
+  MenuList,
+  Box,
+} from "@chakra-ui/react";
 import { Alert, AlertIcon } from "@chakra-ui/react";
 import Toasts from "./toasts/toastd";
 import Toastss from "./toasts/toastw";
 import Toastsss from "./toasts/toastm";
 import Toastssss from "./toasts/toastm2";
 import Toasti from "./toasts/toasti";
+import dynamic from "next/dynamic";
 
 const Avatars = () => {
   return (
     <Stack direction={{ base: "column", lg: "row" }}>
-      <Button variant="unstyled" size="sm" pr={{ lg: "150px" }}>
-        {" "}
-        <Menu>
-          <MenuButton>
-            {" "}
-            <AiFillInstagram size={100} />
-          </MenuButton>
-          <Portal>
-            <></>
-            <MenuList>
-              <MenuItem
-                onClick={() => navigator.clipboard.writeText("austin_ed7")}
-              >
-                <Toasti />
-              </MenuItem>
-              <Alert>
-                <AlertIcon />
-                Click to save it!
-              </Alert>
-            </MenuList>
-          </Portal>
-        </Menu>
-      </Button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <Box>
+        <Button variant="unstyled" size="sm" pr={{ lg: "150px" }}>
+          {" "}
+          <Menu>
+            <MenuButton>
+              {" "}
+              <AiFillInstagram size={100} />
+            </MenuButton>
+            <Portal>
+              <></>
+              <MenuList>
+                <MenuItem
+                  onClick={() => navigator.clipboard.writeText("austin_ed7")}
+                >
+                  <Toasti />
+                </MenuItem>
+                <Alert>
+                  <AlertIcon />
+                  Click to save it!
+                </Alert>
+              </MenuList>
+            </Portal>
+          </Menu>
+        </Button>
+      </Box>
 
-      <Link
-        href="https://www.linkedin.com/in/austin-yang-3544061ba/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <Box>
+        <Link
+          href="https://www.linkedin.com/in/austin-yang-3544061ba/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button
+            variant="unstyled"
+            size="sm"
+            pr={{ lg: "150px" }}
+            pt={{ base: "150px", lg: "0px" }}
+          >
+            {" "}
+            <AiFillLinkedin size={100} />
+          </Button>
+        </Link>
+      </Box>
+
+      <Box>
         <Button
           variant="unstyled"
           size="sm"
-          pr={{ lg: "150px" }}
-          pt={{ lg: "0px" }}
+          pr={{ lg: "70px" }}
+          pt={{ base: "150px", lg: "0px" }}
         >
           {" "}
-          <AiFillLinkedin size={100} />
+          <Menu>
+            <MenuButton>
+              {" "}
+              <AiFillPhone size={100} />
+            </MenuButton>
+            <Portal>
+              <MenuList>
+                <MenuItem
+                  onClick={() => navigator.clipboard.writeText("4164768350")}
+                >
+                  <Toastss />
+                </MenuItem>
+                <Alert>
+                  <AlertIcon />
+                  Click to save it!
+                </Alert>
+              </MenuList>
+            </Portal>
+          </Menu>
         </Button>
-      </Link>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      </Box>
 
-      <Button
-        variant="unstyled"
-        size="sm"
-        pr={{ lg: "70px" }}
-        pt={{ lg: "0px" }}
-      >
-        {" "}
-        <Menu>
-          <MenuButton>
-            {" "}
-            <AiFillPhone size={100} />
-          </MenuButton>
-          <Portal>
-            <MenuList>
-              <MenuItem
-                onClick={() => navigator.clipboard.writeText("4164768350")}
-              >
-                <Toastss />
-              </MenuItem>
-              <Alert>
-                <AlertIcon />
-                Click to save it!
-              </Alert>
-            </MenuList>
-          </Portal>
-        </Menu>
-      </Button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Button
-        variant="unstyled"
-        size="sm"
-        pl={{ lg: "100px" }}
-        pt={{ lg: "0px" }}
-      >
-        {" "}
-        <Menu>
-          <MenuButton>
-            {" "}
-            <Image
-              boxSize="300px"
-              src="/images/discord.png"
-              alt="Discord Icon"
-              width={100}
-              height={100}
-            />
-          </MenuButton>
-          <Portal>
-            <MenuList>
-              <MenuItem
-                onClick={() => navigator.clipboard.writeText("Vivini#6145")}
-              >
-                <Toasts />
-              </MenuItem>
-              <Alert>
-                <AlertIcon />
-                Click to save it!
-              </Alert>
-            </MenuList>
-          </Portal>
-        </Menu>
-      </Button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Button
-        variant="unstyled"
-        size="sm"
-        pl={{ lg: "200px" }}
-        pt={{ lg: "0px" }}
-      >
-        {" "}
-        <Menu>
-          <MenuButton>
-            {" "}
-            <AiFillMail size={100} />
-          </MenuButton>
-          <Portal>
-            <MenuList>
-              <MenuItem
-                onClick={() =>
-                  navigator.clipboard.writeText("au.yang@mail.utoronto.ca")
-                }
-              >
-                <Toastsss />
-              </MenuItem>
-              <MenuItem
-                onClick={() =>
-                  navigator.clipboard.writeText("austinjas014@gmail.com")
-                }
-              >
-                <Toastssss />
-              </MenuItem>
-              <Alert>
-                <AlertIcon />
-                Click to save it!
-              </Alert>
-            </MenuList>
-          </Portal>
-        </Menu>
-      </Button>
+      <Box>
+        <Button
+          variant="unstyled"
+          size="sm"
+          pl={{ lg: "100px" }}
+          pt={{ base: "150px", lg: "0px" }}
+        >
+          {" "}
+          <Menu>
+            <MenuButton>
+              {" "}
+              <Image
+                boxSize="300px"
+                src="/images/discord.png"
+                alt="Discord Icon"
+                width={100}
+                height={100}
+              />
+            </MenuButton>
+            <Portal>
+              <MenuList>
+                <MenuItem
+                  onClick={() => navigator.clipboard.writeText("Vivini#6145")}
+                >
+                  <Toasts />
+                </MenuItem>
+                <Alert>
+                  <AlertIcon />
+                  Click to save it!
+                </Alert>
+              </MenuList>
+            </Portal>
+          </Menu>
+        </Button>
+      </Box>
+
+      <Box>
+        <Button
+          variant="unstyled"
+          size="sm"
+          pl={{ lg: "200px" }}
+          pt={{ base: "150px", lg: "0px" }}
+        >
+          {" "}
+          <Menu>
+            <MenuButton>
+              {" "}
+              <AiFillMail size={100} />
+            </MenuButton>
+            <Portal>
+              <MenuList>
+                <MenuItem
+                  onClick={() =>
+                    navigator.clipboard.writeText("au.yang@mail.utoronto.ca")
+                  }
+                >
+                  <Toastsss />
+                </MenuItem>
+                <MenuItem
+                  onClick={() =>
+                    navigator.clipboard.writeText("austinjas014@gmail.com")
+                  }
+                >
+                  <Toastssss />
+                </MenuItem>
+                <Alert>
+                  <AlertIcon />
+                  Click to save it!
+                </Alert>
+              </MenuList>
+            </Portal>
+          </Menu>
+        </Button>
+      </Box>
       <br></br>
       <br></br>
       <br></br>
@@ -186,4 +186,4 @@ const Avatars = () => {
   );
 };
 
-export default Avatars;
+export default dynamic(() => Promise.resolve(Avatars), { ssr: false });
