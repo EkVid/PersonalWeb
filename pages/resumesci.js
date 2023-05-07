47a5bb5dbf95ff8799665759080ec5e2df7ee9bd
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 15:45:52 -0400

    Update resumesci.js

--- pages/resumesci.js
+++ pages/resumesci.js
@@ -1,11 +1,11 @@
-import Bar from "../components/bar";
+import Navbar from "../components/bar_2";
 import Resumebutton2 from "../components/resume/resbut_2";
 import Scires from "../components/resume/scires";
 
 export default function Home() {
   return (
     <div>
-      <Bar />
+      <Navbar />
       <Scires />
       <br></br>
       <Resumebutton2 />
