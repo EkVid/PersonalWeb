c31c43b80f79828efbab8325499e73f03445dfec
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 15:46:07 -0400

    Update resumetech.js

--- pages/resumetech.js
+++ pages/resumetech.js
@@ -1,11 +1,11 @@
-import Bar from "../components/bar";
+import Navbar from "../components/bar_2";
 import Im from "../components/resume/resume";
 import Resumebutton from "../components/resume/resbut";
 
 export default function Home() {
   return (
     <div>
-      <Bar />
+      <Navbar />
       <Im />
       <br></br>
       <Resumebutton />
