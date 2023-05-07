3a4eb9651e74f0c0369b78648ebdb21a70cd343c
EkVid <118699908+EkVid@users.noreply.github.com>
2023-05-07 16:37:44 -0400

    Update bar_2.js

--- components/bar_2.js
+++ components/bar_2.js
@@ -218,7 +218,7 @@ const Navbar = () => {
                     </Button>
                   </Link>
                   <Link href="/contact">
-                    <Button variant="ghost" leftIcon={<ChatIcon />} size="sm">
+                    <Button variant="ghost" size="sm">
                       Contact
                     </Button>
                   </Link>
