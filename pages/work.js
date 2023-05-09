410fe1ba13981d090ff9f891e3e1c6c31c2249e8
Austin <118699908+EkVid@users.noreply.github.com>
2023-05-08 21:53:08 -0400

    Add files via upload

---
+++ pages/work.js
@@ -0,0 +1,10 @@
+import Bar_Work from "../components/bar_work";
+import Works from "../components/work/Works";
+export default function Home() {
+  return (
+    <div>
+      <Bar_Work />
+      <Works />
+    </div>
+  );
+}
