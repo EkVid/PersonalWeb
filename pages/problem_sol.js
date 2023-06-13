0dfea3c822ba32d22d50a1936148af94e6bac07c
Austin <118699908+EkVid@users.noreply.github.com>
2023-06-13 13:19:04 -0400

    Add files via upload

---
+++ pages/problem_sol.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/bar_2";
+import Effect from "../components/problem_solving/effect";
+
+export default function Problems() {
+  return (
+    <div>
+      <Navbar />
+      <Effect />
+    </div>
+  );
+}
