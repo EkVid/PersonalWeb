8100ef1e70d4f8e5e679892d9dc4b74f32194b81
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:19:17 -0400

    Update work.js

--- pages/work.js
+++ pages/work.js
@@ -1,10 +1,13 @@
 import Bar_Work from "../components/bar_work";
 import Works from "../components/work/Works";
+import ParticleBackground from "../components/background";
+
 export default function Home() {
   return (
     <div>
       <Bar_Work />
       <Works />
+      <ParticleBackground />
     </div>
   );
 }
