8880740adac2ab507cf189f1e375ecf2c179e0b6
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:06:14 -0400

    Added particle effects

--- pages/publications.js
+++ pages/publications.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/bar_2";
 import Anim from "../components/publication/anim";
+import ParticleBackground from "../components/particles/background";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Anim />
+      <ParticleBackground />
     </div>
   );
 }
