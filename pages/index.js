d0f472fc01dc778ae268ee20e6212020ac6f85b0
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:05:42 -0400

    Added particle effects

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Spla from "../components/homepage/moving";
 import Navbar from "../components/bar_2";
+import ParticleBackground from "../components/particles/background";
 
 export default function FirstPost() {
   return (
     <div>
       <Navbar />
       <Spla />
+      <ParticleBackground />
     </div>
   );
 }
