858d287c317ca02b23943a5a7775dba18d02be15
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:11:30 -0400

    Update aboutme.js

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/bar_2";
 import Sp from "../components/aboutme/changess";
-import ParticleBackground from "../components/particles/background";
+import ParticleBackground from "../components/background";
 
 export default function Home() {
   return (
