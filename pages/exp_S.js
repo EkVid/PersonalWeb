0e7079d92a186b9f96b936aa29b3170a0954e660
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:11:43 -0400

    Update exp_S.js

--- pages/exp_S.js
+++ pages/exp_S.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/bar_2";
 import Anim_S from "../components/exp_s/anim_s";
-import ParticleBackground from "../components/particles/background";
+import ParticleBackground from "../components/background";
 
 export default function Home() {
   return (
