53a96ad727afc3880a4ca7c6d429b88c5c74599c
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:05:11 -0400

    Added particle effects

--- pages/exp_S.js
+++ pages/exp_S.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/bar_2";
 import Anim_S from "../components/exp_s/anim_s";
+import ParticleBackground from "../components/particles/background";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Anim_S />
+      <ParticleBackground />
     </div>
   );
 }
