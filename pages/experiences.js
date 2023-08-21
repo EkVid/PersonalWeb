41b22439f763ccd1a7c2d5c31fe829ff02bae2c4
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:11:48 -0400

    Update experiences.js

--- pages/experiences.js
+++ pages/experiences.js
@@ -1,7 +1,7 @@
 import Navbar from "../components/bar_2";
 import Movement from "../components/experience/movement";
 import All from "../components/experience/all";
-import ParticleBackground from "../components/particles/background";
+import ParticleBackground from "../components/background";
 
 export default function Home() {
   return (
