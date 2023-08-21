65f328b68b94604e33edf1aca896091ad9052224
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:11:54 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Spla from "../components/homepage/moving";
 import Navbar from "../components/bar_2";
-import ParticleBackground from "../components/particles/background";
+import ParticleBackground from "../components/background";
 
 export default function FirstPost() {
   return (
