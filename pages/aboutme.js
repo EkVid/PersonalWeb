ba0a8d763e841a1c99cd20686539ada77e514f5c
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:04:39 -0400

    Added particle effects

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/bar_2";
 import Sp from "../components/aboutme/changess";
+import ParticleBackground from "../components/particles/background";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Sp />
+      <ParticleBackground />
     </div>
   );
 }
