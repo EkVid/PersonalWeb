6860a83d2c22fe3cff0a3959148317658352e19a
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:05:59 -0400

    Added particle effects

--- pages/problem_sol.js
+++ pages/problem_sol.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/bar_2";
 import Effect from "../components/problem_solving/effect";
+import ParticleBackground from "../components/particles/background";
 
 export default function Problems() {
   return (
     <div>
       <Navbar />
       <Effect />
+      <ParticleBackground />
     </div>
   );
 }
