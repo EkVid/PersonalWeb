794769ee4ecdc4a019cc52b99b9010d74195bcd4
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:18:49 -0400

    Update publications.js

--- pages/publications.js
+++ pages/publications.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/bar_2";
 import Anim from "../components/publication/anim";
-import ParticleBackground from "../components/particles/background";
+import ParticleBackground from "../components/background";
 
 export default function Home() {
   return (
