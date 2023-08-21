b42836ea1d23637edc6b22c54cc01b16fddd32f2
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:18:44 -0400

    Update problem_sol.js

--- pages/problem_sol.js
+++ pages/problem_sol.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/bar_2";
 import Effect from "../components/problem_solving/effect";
-import ParticleBackground from "../components/particles/background";
+import ParticleBackground from "../components/background";
 
 export default function Problems() {
   return (
