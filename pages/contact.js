d5f8f68369bf9819576926102a35722d45b789ae
Austin <118699908+EkVid@users.noreply.github.com>
2023-08-20 21:04:58 -0400

    Added particle effects

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/bar_2";
 import Mov from "../components/contact/mov";
+import ParticleBackground from "../components/particles/background";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Mov />
+      <ParticleBackground />
     </div>
   );
 }
